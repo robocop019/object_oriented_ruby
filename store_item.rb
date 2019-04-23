65633e68d9624789bbd5536e63d095ce052189ef
robocop019 <chrisbowen@comcast.net>
2019-04-23 11:26:37 -0500

    Created file

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+#Javascript Syntax
+item1 = {item: "Cellphone", color: "Black", price: 500}
+item2 = {item: "Laptop", color: "Silver", price: 2000}
+item3 = {item: "Xbox", color: "White", price: 350}
+
+#Ruby Syntax
+item1 = {:item => "Cellphone", :color => "Black", :price => 500}
+item2 = {:item => "Laptop", :color => "Silver", :price => 2000}
+item3 = {:item => "Xbox", :color => "White", :price => 350}
